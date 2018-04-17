3e9a6d338aa80108b9cee4492ac49e7d62c41aa2
Austin Yu <austinyu@Austins-MacBook-Air.local>
2018-04-17 14:28:57 -0400

    Added model_select

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -3,3 +3,15 @@ generate_data <- function(n, p) {
   responses = rnorm(n)
   return(list(covariates = covariates, responses = responses))
 }
+
+model_select <- function(covariates, responses, cutoff) {
+  summary_lm = summary(lm(responses ~ covariates))[["coefficients"]]
+  p_values = summary_lm[, "Pr(>|t|)"][-1]
+  keep = which(p_values <= cutoff)
+  
+  if (length(keep) == 0) return(vector("numeric"))
+  
+  new_summary = summary(lm(responses ~ covariates[,keep]))[["coefficients"]]
+  new_p_values = new_summary[, "Pr(>|t|)"]
+  return(new_p_values)
+}
